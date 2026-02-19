b2aa6ce48c4123b7adf4b86d036198731a3cce9c
yunikpandey <yunikpd@gmail.com>
2026-02-19 12:01:45 +0545

    Added chat_list for user to write message

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from chat_app.models import Message
+
+
+def chat_list(request):
+    chats= Message.objects.all()
+    return render(
+        request,
+        "chat_list.html",
+        {"chats":chats}
+    )
